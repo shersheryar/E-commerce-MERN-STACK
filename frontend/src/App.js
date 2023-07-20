e603a5e6146836628bdf43b608dc3ed04f218186
SheryarSher <p218027@pwr.nu.edu.pk>
2023-07-20 22:18:08 +0500

    added Navbar to project and paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import logo from "./logo.svg";
+import Nav from "./Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>} />
+          <Route path="/add" element={<h1>Add Product Component</h1>} />
+          <Route path="/update" element={<h1>Update Product Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
