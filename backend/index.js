609d907b50b437a2bae7689d9a3886c7c8111f22
SheryarSher <p218027@pwr.nu.edu.pk>
2023-07-21 14:28:28 +0500

    installed cors module

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 require("./db/config");
 const User = require("./db/User");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.post("/signup", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
