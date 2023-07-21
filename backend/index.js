b0d0bc9dc635b6e2ae322426b50bf9d15900cfd2
SheryarSher <p218027@pwr.nu.edu.pk>
2023-07-21 16:27:12 +0500

    post api for login and remove password from response of post api of signup

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,25 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
 app.post("/signup", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
 });
 
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No User Found" });
+    }
+  } else {
+    res.send({ result: "No User Found" });
+  }
+});
 app.listen(5005);
