aff09fc63d63370f52c2c80e387b6f7417232362
SheryarSher <p218027@pwr.nu.edu.pk>
2023-07-24 22:52:39 +0500

    created a new collection products created backend api for products

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/config");
 const User = require("./db/User");
+const Product = require("./db/Product");
 const app = express();
 const cors = require("cors");
 
@@ -26,5 +27,11 @@ app.post("/login", async (req, res) => {
   } else {
     res.send({ result: "No User Found" });
   }
+});
+
+app.post("/add-product", async (req, res) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  res.send(result);
 });
 app.listen(5005);
