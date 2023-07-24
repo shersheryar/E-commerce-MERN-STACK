2d4c3c59a31cd7e25fb438922534a5036046c3a9
SheryarSher <p218027@pwr.nu.edu.pk>
2023-07-24 22:17:24 +0500

    updated navbar added logo separated navbar to login and signup and the rest of navs

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -9,39 +9,41 @@ const Nav = () => {
   };
   return (
     <div>
-      
-      <ul className="nav-bar">
-        <li>
-          <Link to={"/"}>Products</Link>
-        </li>
-        <li>
-          <Link to={"/add"}>Add Products</Link>
-        </li>
-        <li>
-          <Link to={"/update"}>Update Products</Link>
-        </li>
-        <li>
-          <Link to={"/profile"}>Profile</Link>
-        </li>
-
-        {auth ? (
+      <img
+        className="logo"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.K2g31QXAf6Z5F2osqI_n_AHaGL%26pid%3DApi&f=1&ipt=6fde0f81a77d16686d47176ead6004c2e9c626114bfb4d62c2eb2a4983bbe35a&ipo=images"
+        alt="logo"
+      />
+      {auth ? (
+        <ul className="nav-bar">
           <li>
-            {" "}
+            <Link to={"/"}>Products</Link>
+          </li>
+          <li>
+            <Link to={"/add"}>Add Products</Link>
+          </li>
+          <li>
+            <Link to={"/update"}>Update Products</Link>
+          </li>
+          <li>
+            <Link to={"/profile"}>Profile</Link>
+          </li>
+          <li>
             <Link onClick={logout} to={"/login"}>
-              Logout
+              Logout ({JSON.parse(auth).name})
             </Link>
           </li>
-        ) : (
-          <>
-            <li>
-              <Link to={"/signup"}>Sign up</Link>
-            </li>
-            <li>
-              <Link to={"/login"}>Login</Link>
-            </li>
-          </>
-        )}
-      </ul>
+        </ul>
+      ) : (
+        <ul className="nav-bar nav-right">
+          <li>
+            <Link to={"/signup"}>Sign up</Link>
+          </li>
+          <li>
+            <Link to={"/login"}>Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
