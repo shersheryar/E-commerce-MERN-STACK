27ac5b9b7f6abb1ec23f86beaf857e8d65d91772
SheryarSher <p218027@pwr.nu.edu.pk>
2023-07-25 23:09:24 +0500

    prefill update form integrated api of prefill form

--- frontend/src/components/UpdateProduct.js
+++ frontend/src/components/UpdateProduct.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 const AddProduct = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
+  const params = useParams();
 
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
+  const getProductDetail = async () => {
+    let result = await fetch(`http://localhost:5005/product/${params.id}`);
+    result = await result.json();
+    setName(result.name);
+    setPrice(result.price);
+    setCategory(result.category);
+    setCompany(result.company);
+  };
   const updateProduct = async () => {
     // console.log(name, price, category, company);
     const userId = JSON.parse(localStorage.getItem("user"))._id;
@@ -18,7 +31,7 @@ const AddProduct = () => {
       },
     });
     result = await result.json();
-    console.log(result);
+    // console.log(result);
   };
   return (
     <div className="add-product">
