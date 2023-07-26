da0bc8a15935a68c20fa747c5d0a86f6828f9e36
SheryarSher <p218027@pwr.nu.edu.pk>
2023-07-26 17:42:20 +0500

    search api for searching

--- backend/index.js
+++ backend/index.js
@@ -65,4 +65,16 @@ app.put("/product/:id", async (req, res) => {
   );
   res.send(result);
 });
+
+app.get("/search/:key", async (req, res) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { price: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+    ],
+  });
+  res.send(result);
+});
 app.listen(5005);
